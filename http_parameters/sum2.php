a2c944195c9d09f5cbef20b82168a0567564315d
GambuzX <mg_mesquita@hotmail.com>
2019-10-16 10:17:01 +0100

    Sqlite database

--- http_parameters/sum2.php
+++ http_parameters/sum2.php
@@ -4,5 +4,6 @@
     $num2 = $_GET['num2'];
     $sum = $num1 + $num2;
     echo "<p>$num1 + $num2 = $sum</p>";
-
 ?>
+
+<a href="form2.html">Go back to form</a>
---
+++ sqlite_database/list_news.php
@@ -0,0 +1,15 @@
+<?php
+
+    $db = new PDO('sqlite:news.db');
+
+    $stmt = $db->prepare('select * from news');
+    $stmt->execute();
+
+    $articles = $stmt->fetchAll();
+
+    foreach($articles as $article) {
+        echo '<h1>' . $article['title'] . '</h1>';
+        echo '<p>' . $article['introduction'] . '</p>';
+    }
+
+?>
